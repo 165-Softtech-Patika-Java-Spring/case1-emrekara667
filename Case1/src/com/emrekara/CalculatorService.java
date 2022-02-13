179d619822c8d9cfc0ed2e9babfe82759252656f
Emre Kara <emre_kara_97@hotmail.com>
2022-02-13 17:05:07 +0300

    CalculatorService update

--- Case1/src/com/emrekara/CalculatorService.java
+++ Case1/src/com/emrekara/CalculatorService.java
@@ -8,111 +8,105 @@ import java.util.Iterator;
 import java.util.List;
 
 public class CalculatorService {
-    public CalculatorService() {
-    }
 
-    public static BigDecimal totalPriceHouse(List<RealEstate> listHouse) {
+
+    public  BigDecimal totalPriceHouse(List<RealEstate> listHouse) {
         BigDecimal totalHousePrice = BigDecimal.ZERO;
 
-        RealEstate houseTemp;
-        for(Iterator var2 = listHouse.iterator(); var2.hasNext(); totalHousePrice = totalHousePrice.add(houseTemp.getPrice())) {
-            houseTemp = (RealEstate)var2.next();
+        for(RealEstate houseTemp : listHouse){
+            totalHousePrice = totalHousePrice.add(houseTemp.getPrice());
         }
 
         return totalHousePrice;
     }
 
-    public static BigDecimal totalPriceVilla(List<RealEstate> listVilla) {
-        BigDecimal totalHousePrice = BigDecimal.ZERO;
+    public  BigDecimal totalPriceVilla(List<RealEstate> listVilla) {
+        BigDecimal totalVillaPrice = BigDecimal.ZERO;
 
-        RealEstate villaTemp;
-        for(Iterator var2 = listVilla.iterator(); var2.hasNext(); totalHousePrice = totalHousePrice.add(villaTemp.getPrice())) {
-            villaTemp = (RealEstate)var2.next();
+        for(RealEstate villaTemp : listVilla){
+            totalVillaPrice = totalVillaPrice.add(villaTemp.getPrice());
         }
 
-        return totalHousePrice;
+        return totalVillaPrice;
     }
 
-    public static BigDecimal totalPriceSummerHouse(List<RealEstate> listSummerHouse) {
-        BigDecimal totalHousePrice = BigDecimal.ZERO;
+    public  BigDecimal totalPriceSummerHouse(List<RealEstate> listSummerHouse) {
+        BigDecimal totalSummerHousePrice = BigDecimal.ZERO;
 
-        RealEstate summerHouseTemp;
-        for(Iterator var2 = listSummerHouse.iterator(); var2.hasNext(); totalHousePrice = totalHousePrice.add(summerHouseTemp.getPrice())) {
-            summerHouseTemp = (RealEstate)var2.next();
+        for(RealEstate villaTemp : listSummerHouse){
+            totalSummerHousePrice = totalSummerHousePrice.add(villaTemp.getPrice());
         }
 
-        return totalHousePrice;
+        return totalSummerHousePrice;
     }
 
-    public static BigDecimal totalPriceAllEstate(List<RealEstate> listEstate) {
+    public  BigDecimal totalPriceAllEstate(List<RealEstate> listEstate) {
         BigDecimal totalEstatePrice = BigDecimal.ZERO;
 
-        RealEstate estateTemp;
-        for(Iterator var2 = listEstate.iterator(); var2.hasNext(); totalEstatePrice = totalEstatePrice.add(estateTemp.getPrice())) {
-            estateTemp = (RealEstate)var2.next();
+        for(RealEstate realEstateTemp : listEstate){
+            totalEstatePrice = totalEstatePrice.add(realEstateTemp.getPrice());
         }
 
         return totalEstatePrice;
     }
 
-    public static BigDecimal averageSquareMetersofHouse(List<RealEstate> listHouse) {
+    public  BigDecimal averageSquareMetersofHouse(List<RealEstate> listHouse) {
         BigDecimal averageSquaremeter = BigDecimal.ZERO;
-        int numberOfHouses = 0;
+        int numberOfHouse = 0;
 
-        for(Iterator var3 = listHouse.iterator(); var3.hasNext(); ++numberOfHouses) {
-            RealEstate houseTemp = (RealEstate)var3.next();
+        for(RealEstate houseTemp : listHouse){
             averageSquaremeter = averageSquaremeter.add(houseTemp.getSquareMeter());
+            numberOfHouse++;
         }
 
-        return averageSquaremeter.divide(new BigDecimal(numberOfHouses), 2, RoundingMode.HALF_UP);
+        return averageSquaremeter.divide(new BigDecimal(numberOfHouse), 2, RoundingMode.HALF_UP);
     }
 
-    public static BigDecimal averageSquareMetersofVilla(List<RealEstate> listVilla) {
+    public  BigDecimal averageSquareMetersofVilla(List<RealEstate> listVilla) {
         BigDecimal averageSquaremeter = BigDecimal.ZERO;
         int numberOfVillas = 0;
 
-        for(Iterator var3 = listVilla.iterator(); var3.hasNext(); ++numberOfVillas) {
-            RealEstate villaTemp = (RealEstate)var3.next();
+        for(RealEstate villaTemp : listVilla){
             averageSquaremeter = averageSquaremeter.add(villaTemp.getSquareMeter());
+            numberOfVillas++;
         }
 
         return averageSquaremeter.divide(new BigDecimal(numberOfVillas), 2, RoundingMode.HALF_UP);
     }
 
-    public static BigDecimal averageSquareMetersofSummerHouse(List<RealEstate> listSummerHouse) {
+    public  BigDecimal averageSquareMetersofSummerHouse(List<RealEstate> listSummerHouse) {
         BigDecimal averageSquaremeter = BigDecimal.ZERO;
         int numberOfSummerHouse = 0;
 
-        for(Iterator var3 = listSummerHouse.iterator(); var3.hasNext(); ++numberOfSummerHouse) {
-            RealEstate summerHouseTemp = (RealEstate)var3.next();
+        for(RealEstate summerHouseTemp : listSummerHouse){
             averageSquaremeter = averageSquaremeter.add(summerHouseTemp.getSquareMeter());
+            numberOfSummerHouse++;
         }
 
         return averageSquaremeter.divide(new BigDecimal(numberOfSummerHouse), 2, RoundingMode.HALF_UP);
     }
 
-    public static BigDecimal averageSquareMetersofRealEstates(List<RealEstate> listRealEstate) {
+    public  BigDecimal averageSquareMetersofRealEstates(List<RealEstate> listRealEstate) {
         BigDecimal averageSquaremeter = BigDecimal.ZERO;
         int numberOfRealEstate = 0;
 
-        for(Iterator var3 = listRealEstate.iterator(); var3.hasNext(); ++numberOfRealEstate) {
-            RealEstate realEstateTemp = (RealEstate)var3.next();
+        for(RealEstate realEstateTemp : listRealEstate){
             averageSquaremeter = averageSquaremeter.add(realEstateTemp.getSquareMeter());
+            numberOfRealEstate++;
         }
 
         return averageSquaremeter.divide(new BigDecimal(numberOfRealEstate), 2, RoundingMode.HALF_UP);
     }
 
-    public static void getSelectedRealEstate(List<RealEstate> realEstateList, Long numberOfRooms, Long numberOfHalls) {
+    public  void getSelectedRealEstate(List<RealEstate> realEstateList, Long numberOfRooms, Long numberOfHalls) {
         List<RealEstate> realEstateTemp = new ArrayList();
         Iterator var4 = realEstateList.iterator();
 
-        while(var4.hasNext()) {
-            RealEstate realEstate = (RealEstate)var4.next();
-            if (realEstate.getNumberOfHalls().equals(numberOfHalls) && realEstate.getNumberOfRooms().equals(numberOfRooms)) {
-                realEstateTemp.add(realEstate);
-            }
-        }
+       for(RealEstate realEstate: realEstateList){
+           if (realEstate.getNumberOfHalls().equals(numberOfHalls) && realEstate.getNumberOfRooms().equals(numberOfRooms)) {
+               realEstateTemp.add(realEstate);
+           }
+       }
 
         if (realEstateTemp.isEmpty()) {
             System.out.println("This type of real estate as not found ! Rooms : " + numberOfRooms + " Halls : " + numberOfHalls);
@@ -123,15 +117,11 @@ public class CalculatorService {
 
     }
 
-    public static void showRealEstate(List<RealEstate> realEstateList) {
-        Iterator var1 = realEstateList.iterator();
+    public void showRealEstate(List<RealEstate> realEstateList) {
 
-        while(var1.hasNext()) {
-            RealEstate realEstateTemp = (RealEstate)var1.next();
-            PrintStream var10000 = System.out;
-            String var10001 = realEstateTemp.getClass().getSimpleName();
-            var10000.println(var10001 + " : Price : " + realEstateTemp.getPrice() + " Square meter : " + realEstateTemp.getSquareMeter());
-        }
+        for(RealEstate realEstateTemp : realEstateList){
+            System.out.println( realEstateTemp.getClass().getSimpleName() + " : Price : " + realEstateTemp.getPrice() + " Square meter : " + realEstateTemp.getSquareMeter());
+    }
 
     }
 }
--- Case1/src/com/emrekara/RealEstateService.java
+++ Case1/src/com/emrekara/RealEstateService.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RealEstateService {
+
+    private  List<RealEstate> houseList ;
+    private  List<RealEstate> summerHouseList;
+    private  List<RealEstate> villaList;
+    private  List<RealEstate> realEstateList;
+
     private RealEstate house1 = new House(new BigDecimal("250000"), new BigDecimal("120"), 1L, 1L);
     private RealEstate house2 = new House(new BigDecimal("300000"), new BigDecimal("140"), 2L, 1L);
     private RealEstate house3 = new House(new BigDecimal("350000"), new BigDecimal("190"), 3L, 1L);
@@ -19,60 +25,84 @@ public class RealEstateService {
     private RealEstate summerHouse2 = new SummerHouse(new BigDecimal("450000"), new BigDecimal("160"), 4L, 2L);
     private RealEstate summerHouse3 = new SummerHouse(new BigDecimal("475000"), new BigDecimal("190"), 5L, 2L);
 
-
-    public List<RealEstate> getAllEstateList() {
-        List<RealEstate> allEstatelist = new ArrayList();
-        allEstatelist.add(this.house1);
-        allEstatelist.add(this.house2);
-        allEstatelist.add(this.house3);
-        allEstatelist.add(this.villa1);
-        allEstatelist.add(this.villa2);
-        allEstatelist.add(this.villa3);
-        allEstatelist.add(this.summerHouse1);
-        allEstatelist.add(this.summerHouse2);
-        allEstatelist.add(this.summerHouse3);
-        return allEstatelist;
+    public RealEstateService(){
+        setHouseList();
+        setSummerHouselist();
+        setVillaList();
+        setRealEstateList();
     }
 
-    public List<RealEstate> getHouseList() {
-        List<RealEstate> houseList = new ArrayList();
+    public void setRealEstateList(){
+        realEstateList = new ArrayList();
+        realEstateList.add(this.house1);
+        realEstateList.add(this.house2);
+        realEstateList.add(this.house3);
+        realEstateList.add(this.villa1);
+        realEstateList.add(this.villa2);
+        realEstateList.add(this.villa3);
+        realEstateList.add(this.summerHouse1);
+        realEstateList.add(this.summerHouse2);
+        realEstateList.add(this.summerHouse3);
+    }
+
+
+    public List<RealEstate> getRealEstateList() {
+        return realEstateList;
+    }
+
+    public void setHouseList(){
+        houseList = new ArrayList<>();
         houseList.add(this.house1);
         houseList.add(this.house2);
         houseList.add(this.house3);
+    }
+
+    public List<RealEstate> getHouseList() {
         return houseList;
     }
 
-    public List<RealEstate> getVillaList() {
-        List<RealEstate> villaList = new ArrayList();
+    public void setVillaList(){
+        villaList = new ArrayList<>();
         villaList.add(this.villa1);
         villaList.add(this.villa2);
         villaList.add(this.villa3);
+    }
+
+    public List<RealEstate> getVillaList() {
         return villaList;
     }
 
-    public List<RealEstate> getSummerHouseList() {
-        List<RealEstate> summerHouseList = new ArrayList();
+    public void setSummerHouselist(){
+        summerHouseList = new ArrayList<>();
         summerHouseList.add(this.summerHouse1);
         summerHouseList.add(this.summerHouse2);
         summerHouseList.add(this.summerHouse3);
+    }
+
+    public List<RealEstate> getSummerHouseList() {
         return summerHouseList;
     }
 
     public static void main(String[] args) {
+
         RealEstateService realEstateService = new RealEstateService();
+        CalculatorService calculatorService = new CalculatorService();
+
         List<RealEstate> houseList = realEstateService.getHouseList();
         List<RealEstate> villaList = realEstateService.getVillaList();
         List<RealEstate> summerHouseList = realEstateService.getSummerHouseList();
-        List<RealEstate> realEstateList = realEstateService.getAllEstateList();
-        System.out.println("Total price of houses : " + CalculatorService.totalPriceHouse(houseList));
-        System.out.println("Total price of villas :  " + CalculatorService.totalPriceVilla(villaList));
-        System.out.println("total price of summer houses : " + CalculatorService.totalPriceSummerHouse(summerHouseList));
-        System.out.println("Total price of all real estate : " + CalculatorService.totalPriceAllEstate(realEstateList));
-        System.out.println("Average square meter of Houses : " + CalculatorService.averageSquareMetersofHouse(houseList));
-        System.out.println("Average square meter of Villas : " + CalculatorService.averageSquareMetersofVilla(villaList));
-        System.out.println("Average square meter of Summer Houses : " + CalculatorService.averageSquareMetersofSummerHouse(summerHouseList));
-        System.out.println("Average square meter of all Real Estate : " + CalculatorService.averageSquareMetersofRealEstates(realEstateList));
-        CalculatorService.getSelectedRealEstate(realEstateList, 7L, 2L);
-        CalculatorService.getSelectedRealEstate(realEstateList, 10L, 2L);
+        List<RealEstate> realEstateList = realEstateService.getRealEstateList();
+
+
+        System.out.println("Total price of houses : " + calculatorService.totalPriceHouse(houseList));
+        System.out.println("Total price of villas :  " + calculatorService.totalPriceVilla(villaList));
+        System.out.println("total price of summer houses : " + calculatorService.totalPriceSummerHouse(summerHouseList));
+        System.out.println("Total price of all real estate : " + calculatorService.totalPriceAllEstate(realEstateList));
+        System.out.println("Average square meter of Houses : " + calculatorService.averageSquareMetersofHouse(houseList));
+        System.out.println("Average square meter of Villas : " + calculatorService.averageSquareMetersofVilla(villaList));
+        System.out.println("Average square meter of Summer Houses : " + calculatorService.averageSquareMetersofSummerHouse(summerHouseList));
+        System.out.println("Average square meter of all Real Estate : " + calculatorService.averageSquareMetersofRealEstates(realEstateList));
+        calculatorService.getSelectedRealEstate(realEstateList, 7L, 2L);
+        calculatorService.getSelectedRealEstate(realEstateList, 10L, 2L);
     }
 }
